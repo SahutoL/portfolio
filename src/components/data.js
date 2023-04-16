1926a99b5e3aec33885d14525b0a71ac6368f1a2
SahutoL <96646448+SahutoL@users.noreply.github.com>
2023-04-16 14:22:11 +0900

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -6,12 +6,13 @@ export const projectData = [
         'A simple Todo app built with React, Next.js',
       gitHubLink: 'https://github.com/SahutoL/nextjs-todo',
       siteUrl: 'https://nextjs-todo-virid.vercel.app/'
-    },/* 
+    },
     {
       id: 2,
-      title: 'Books Library App',
+      title: 'Hameln API',
       description:
-        'A simple Book Library App built with JavaScript. It helps readers have a good list of books they are either currently reading or have finished reading.',
-      gitHubLink: 'https://github.com/olawanlejoel/Book-Library',
-    }, */
+        'This is the ranking API for "syosetu.org".',
+      gitHubLink: 'https://github.com/SahutoL/hameln-api',
+      siteUrl: 'https://hameln-api.onrender.com/docs/'
+    },
   ];
